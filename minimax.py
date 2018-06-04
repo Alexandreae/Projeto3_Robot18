d5d126cc89d66c49225c1f7faeff08989af446f1
JoseFernandoInsper <josefmc@al.edu.br>
2018-06-04 15:31:09 -0300

    minimax esqueleto

---
+++ minimax.py
@@ -0,0 +1,31 @@
+import math
+
+class AISolver(object):
+    """docstring for AISolver."""
+    def __init__(self, arg):
+        super(AISolver, self).__init__()
+        self.arg = arg
+
+    def Player():
+
+        
+
+    def direction FindBestMove():
+
+        return direction
+
+    def minimax():
+
+        return result
+
+    def alphabeta():
+
+        return result
+
+    def heuristicScore():
+
+        return math.max(score, math.min(actualScore, 1))
+
+    def calculateClusteringScore():
+
+        return clusteringScore
